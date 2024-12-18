9a390c46c4afc9c5c7fefb32b842e33cb1b3d234
hung6033 <90978454+hung6033@users.noreply.github.com>
2024-12-18 10:04:14 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Dear',
+    introDesc: `Tớ biết tụi mình luôn luôn lựa chọn im lặng và biến mất, nhưng cậu có thể đừng bỏ rơi tớ được không
+`,
+    btnIntro: '...',
+    title: 'Không một giọt nước mắt nào có thể ngăn một người không muốn ở lại',
+    desc: 'Nhưng dù vậy tớ cũng không muốn tụi mình im lặng như này ',
+    btnYes: 'Vậy nên hãy nói chuyện với tớ nhé?',
+    btnNo: 'Đéo',
+    question:'Tớ sẽ không làm cậu tổn thương hay làm cậu phải khó chịu thêm lần nào nữa đâu',
+    btnReply: 'Gửi',
+    reply: 'Được rồi...',
+    mess: 'Sẽ không còn cãi vã nữa đâu',
+    messDesc: 'I Miss You',
+    btnAccept: '...',
+    messLink: 'https://www.facebook.com/messages/t/100051992931764' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
